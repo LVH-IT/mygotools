264cacc7bc0d1396856c9239e93cd4c3fc523f45
G4mer1808 <g4mer1808@gmail.com>
2021-03-25 18:45:35 +0100

    ...

--- mygotools.go
+++ mygotools.go
@@ -3,18 +3,33 @@ package mygotools
 import (
 	"bufio"
 	"os"
+	"os/exec"
 	"runtime"
 	"strings"
 )
+
+var hostOS = runtime.GOOS
 
 func GetUserInput(printText string) string {
 	println(printText)
 	reader := bufio.NewReader(os.Stdin)
 	userInput, _ := reader.ReadString('\n')
-	if runtime.GOOS == "windows" {
+	if hostOS == "windows" {
 		userInput = strings.TrimSuffix(userInput, "\r\n")
 	} else {
 		userInput = strings.TrimSuffix(userInput, "\n")
 	}
 	return userInput
+}
+
+func ClearCLI() {
+	var clear *exec.Cmd
+	if hostOS == "windows" {
+		clear = exec.Command("cmd", "/c", "cls")
+	}
+	if hostOS == "linux" {
+		clear = exec.Command("clear")
+	}
+	clear.Stdout = os.Stdout
+	clear.Run()
 }
